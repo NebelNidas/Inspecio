bac743ffe43d9d604e65f79203fe461c65ede632
Julian Burner <burnerjulian@gmail.com>
2022-07-07 13:37:38 +0200

    Remove unused imports

--- src/main/java/io/github/queerbric/inspecio/InspecioCommand.java
+++ src/main/java/io/github/queerbric/inspecio/InspecioCommand.java
@@ -23,7 +23,6 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.tree.LiteralCommandNode;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.text.MutableText;
